959e76068e3eac216915088f4d0515922a8b1710
Paul Bonser <pib@paulbonser.com>
2019-12-18 19:39:31 -0600

    tell us what is wrong unzip

--- src/main.sh
+++ src/main.sh
@@ -85,7 +85,7 @@ function installTerraform {
   echo "Successfully downloaded Terraform v${tfVersion}"
 
   echo "Unzipping Terraform v${tfVersion}"
-  unzip -d /usr/local/bin /tmp/terraform_${tfVersion} &> /dev/null
+  unzip -v -d /usr/local/bin /tmp/terraform_${tfVersion}
   if [ "${?}" -ne 0 ]; then
     echo "Failed to unzip Terraform v${tfVersion}"
     exit 1
